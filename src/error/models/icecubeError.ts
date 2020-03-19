b78c869d0456a64b8b03a80b38fadee8c63a8739
lior bahar <lior.bahar1412@gmail.com>
2020-03-19 14:10:18 +0200

    fix

--- src/error/models/icecubeError.ts
+++
@@ -1,8 +0,0 @@
-export class IcecubeError extends Error {
-    constructor(message: string) {
-      super(message);
-      this.name = "IcecubeError";
-      Error.captureStackTrace(this, IcecubeError);
-    }
-  }
-  
